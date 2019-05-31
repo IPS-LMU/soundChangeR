4642d396dfeb956e23e27566a163c99ce10a6788
Johanna Cronenberg <johanna.cronenberg@campus.lmu.de>
2019-05-31 13:21:50 +0200

    refactoring

--- functions/simulations.R
+++ functions/simulations.R
@@ -25,8 +25,7 @@ savePopulation <- function(pop, extraCols = list(condition = "x"), logDir) {
   
   dir.create(logDir, showWarnings = FALSE, recursive = TRUE)
   saveRDS(convert_pop_list_to_dt(pop, extraCols),
-          file = file.path(logDir, paste("pop", unlist(extraCols), "rds", sep = "."))
-  )
+          file = file.path(logDir, paste("pop", unlist(extraCols), "rds", sep = ".")))
 }
 
 saveInteractionsLog <- function(interactionsLog, logDir) {
