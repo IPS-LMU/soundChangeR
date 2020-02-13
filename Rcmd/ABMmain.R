347364c696c70b58e624754fd99b4d07dec02cff
johannacronenberg <johanna.cronenberg@web.de>
2020-02-13 12:03:24 +0100

    removed setting the working directory (see github issue) and did some refactoring

--- Rcmd/ABMmain.R
+++ Rcmd/ABMmain.R
@@ -9,10 +9,6 @@
 #                                                                              #
 ################################################################################
 
-# set path
-ABMpath <- "/homes/m.gubian/ABM/ABM"
-setwd(ABMpath)
-
 # load libraries and parameters
 source(file.path("Rcmd", "loadLibraries.R"))
 source(file.path("data", "params.R"))
@@ -22,12 +18,12 @@ dir.create(params[["rootLogDir"]], showWarnings = FALSE, recursive = TRUE)
 create_simulation_register(params[["rootLogDir"]])
 
 # create logging directory for this specific simulation
-params[['simulationName']] <- generate_simulation_name()
-logDir <- file.path(params[["rootLogDir"]], params[['simulationName']])
+params[["simulationName"]] <- generate_simulation_name()
+logDir <- file.path(params[["rootLogDir"]], params[["simulationName"]])
 dir.create(logDir, showWarnings = FALSE, recursive = TRUE)
 
 # load input data.table
-input.df <- suppressWarnings(fread(params[['inputDataFile']], stringsAsFactors = F))
+input.df <- suppressWarnings(fread(params[["inputDataFile"]], stringsAsFactors = F))
 input.df %>% setnames(c(params[["word"]], params[["speaker"]], params[["group"]]), c("word", "speaker", "group"))
 input.df$initial <- input.df[, params[["initial"]], with = FALSE]
 input.df$label <- input.df[, params[["label"]], with = FALSE]
@@ -37,38 +33,37 @@ if (!is.null(params[["subsetSpeakers"]])) {
 if (!is.null(params[["subsetLabels"]])) {
   input.df <- input.df[label %in% params[["subsetLabels"]]]
 }
-set_feature_names(input.df, params[['features']])
+set_feature_names(input.df, params[["features"]])
 
 # save input.df
 saveRDS(input.df, file.path(logDir, "input.rds"))
 
 # save commit hash so that it is known which version of the ABM was used here
-params[['commitHash']] <- system("git log -n1 --format=format:\"%H\"", intern = TRUE)
+params[["commitHash"]] <- system("git log -n1 --format=format:\"%H\"", intern = TRUE)
 
 # log simulation in register and save params
 register_simulation(params)
 
 # run simulations
-if (params[['runMode']] == "single") {
-  coreABM(input.df, params, logDir)
-} else if (params[['runMode']] == "multiple") {
+if (params[["runMode"]] == "single") {
+  coreABM(input.df, params, file.path(logDir, "1"))
+} else if (params[["runMode"]] == "multiple") {
   require(parallel)
   numCores <- detectCores() - 1
-  if (Sys.info()[['sysname']] == "Windows") {
+  if (Sys.info()[["sysname"]] == "Windows") {
     cl <- makeCluster(numCores, type = "PSOCK")
-    clusterExport(cl, c("input.df", "params", "ABMpath", "logDir"))
+    clusterExport(cl, c("input.df", "params", "logDir"))
     clusterEvalQ(cl, {
-      setwd(ABMpath)
       source(file.path("Rcmd", "loadLibraries.R"))
     })
   } else {
     cl <- makeCluster(numCores, type = "FORK")
   }
   clusterSetRNGStream(cl)
-  parLapply(cl, seq_len(params[['multipleABMRuns']]), function(abmName) {
+  parLapply(cl, seq_len(params[["multipleABMRuns"]]), function(abmName) {
     coreABM(input.df, params, file.path(logDir, abmName))
   })
   stopCluster(cl)
 }
-set_completed(params[['simulationName']], params[["rootLogDir"]])
+set_completed(params[["simulationName"]], params[["rootLogDir"]])
 
