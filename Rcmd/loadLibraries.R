bffd5a2a34d88456dc5a0642e85a1309f2d32a51
Johanna Cronenberg <johanna.cronenberg@campus.lmu.de>
2020-08-05 12:11:19 +0200

    refactoring of loadLibraries

--- Rcmd/loadLibraries.R
+++ Rcmd/loadLibraries.R
@@ -87,14 +87,12 @@ coreABM <- function(input.df, params, logDir) {
   
   pop <- create_population(input.df = input.df, params = params)
   
-  if (params[['splitAndMerge']] == TRUE & params[['doSplitAndMergeBeforeABM']] == TRUE) {
+  if (params[["splitAndMerge"]] == TRUE & params[["doSplitAndMergeBeforeABM"]] == TRUE) {
     for (j in seq_along(pop)) {
       splitandmerge(pop[[j]], params, full = TRUE)
     }
   }
-  save_population(pop,
-                 extraCols = list(snapshot = 0),
-                 logDir = logDir)
+  save_population(pop, extraCols = list(snapshot = 0), logDir = logDir)
   
   perform_interactions(pop, logDir, params)
 }
