7770e9f20b6b9c2b066d7832548113feab4a653e
Johanna Cronenberg <johanna.cronenberg@campus.lmu.de>
2021-10-23 20:22:11 +0200

    added checks for all important parameters

--- R/check_params.R
+++ R/check_params.R
@@ -10,66 +10,207 @@
 check_params <- function(params, input.df) {
 
   runSimulation <- TRUE
-
-  if (base::is.null(params[["initialMemoryResampling"]])) {
+  
+  if (!"createPopulationMethod" %in% base::names(params) || base::is.null(params[["createPopulationMethod"]])) {
+    params[["createPopulationMethod"]] <- "speaker_is_agent"
+  }
+  if (params[["createPopulationMethod"]] == "bootstrap" && 
+      (params[["bootstrapPopulationSize"]] <= 0 || !"bootstrapPopulationSize" %in% base::names(params))) {
+    write_log("Please specify parameter bootstrapPopulationSize correctly (number or named vector of numbers higher than zero).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"initialMemoryResampling" %in% base::names(params) || base::is.null(params[["initialMemoryResampling"]])) {
     params[["initialMemoryResampling"]] <- FALSE
   }
+  
   if (params[["initialMemoryResampling"]]) {
-    if (base::is.null(params[["initialMemoryResamplingFactor"]])) {
-      params[["initialMemoryResamplingFactor"]] <- 1
-    } else if (!base::is.numeric(params[["initialMemoryResamplingFactor"]]) | params[["initialMemoryResamplingFactor"]] <= 0) {
+    if (!"initialMemoryResamplingFactor" %in% base::names(params) ||
+        !base::is.numeric(params[["initialMemoryResamplingFactor"]]) || 
+        params[["initialMemoryResamplingFactor"]] <= 0) {
+      write_log("Please specify parameter initialMemoryResamplingFactor correctly (number higher than zero).", params)
       runSimulation <- FALSE
     }
   }
-  if (base::is.null(params[["removeOriginalExemplarsAfterResampling"]])) {
+  
+  if (!"removeOriginalExemplarsAfterResampling" %in% base::names(params) || 
+      base::is.null(params[["removeOriginalExemplarsAfterResampling"]])) {
     params[["removeOriginalExemplarsAfterResampling"]] <- FALSE
   }
 
+  methodReg <- get_method_register()
+  if (!"featureExtractionMethod" %in% base::names(params) || base::is.null(params[["featureExtractionMethod"]])) {
+    params[["featureExtractionMethod"]] <- "identity"
+  }
+  if (!params[["featureExtractionMethod"]] %in% methodReg$method) {
+    write_log("Your chosen featureExtractionMethod is currently not implemented. Please use featureExtractionMethod: 'identity'.", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"productionBasis" %in% base::names(params)) {
+    params[["productionBasis"]] <- "word"
+  }
+  if (base::grepl("^(target)?[wW]ord$", params[["productionBasis"]])) {
+    params[["productionBasis"]] <- "word"
+  } else if (base::grepl("^(target)?([lL]abel|[pP]honeme)$", params[["productionBasis"]])) {
+    params[["productionBasis"]] <- "label"
+  } else {
+    write_log("Please specify parameter productionBasis correctly (either 'word' or 'label').", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"productionResampling" %in% base::names(params)) {
+    params[["productionResampling"]] <- "SMOTE"
+  }
+  if (!(base::grepl("SMOTE", params[["productionResampling"]], ignore.case = TRUE) || base::is.null(params[["productionResampling"]]))) {
+    write_log("Please specify parameter productionResampling correctly (either 'SMOTE' or NULL).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"productionResamplingFallback" %in% base::names(params)) {
+    params[["productionResamplingFallback"]] <- "label"
+  }
+  if (base::grepl("label|phoneme", params[["productionResamplingFallback"]], ignore.case = TRUE)) {
+    params[["productionResamplingFallback"]] <- "label"
+  }
+  
+  if (!"productionMinTokens" %in% base::names(params) || params[["productionMinTokens"]] <= 0) {
+    write_log("Please specify parameter productionMinTokens correctly (number higher than zero).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"productionSMOTENN" %in% base::names(params) || params[["productionSMOTENN"]] <= 0) {
+    write_log("Please specify parameter productionSMOTENN correctly (number higher than zero).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"perceptionModels" %in% base::names(params) || base::is.null(params[["perceptionModels"]])) {
+    params[["perceptionModels"]] <- "singleGaussian"
+  }
+  if (base::grepl("^GMM(s)?", params[["perceptionModels"]], ignore.case = TRUE)) {
+    params[["perceptionModels"]] <- "GMM"
+  }
+  
+  if (!"memoryIntakeStrategy" %in% base::names(params) || base::is.null(params[["memoryIntakeStrategy"]])) {
+    write_log("", params)
+    runSimulation <- FALSE
+  }
+  if (!base::any(base::c("maxPosteriorProb", "posteriorProbThr", "mahalanobisDistance") %in% params[["memoryIntakeStrategy"]])) {
+    write_log("Please specify parameter memoryIntakeStrategy correctly (string or vector with one/several of 
+              'maxPosteriorProb', 'mahalanobisDistance', 'posteriorProbThr').", params)
+    runSimulation <- FALSE
+  }
   if (base::any(base::c("maxPosteriorProb", "posteriorProbThr") %in% params[["memoryIntakeStrategy"]])) {
-    if (base::is.null(params[["posteriorProbMethod"]])) {
-      if (base::is.null(params[["perceptionModels"]]) || params[["perceptionModels"]] == "singleGaussian") {
-        params[["posteriorProbMethod"]] <- "qda"
-      } else if (base::grepl("^GMM(s)?", params[["perceptionModels"]])) {
-        params[["posteriorProbMethod"]] <- "GMM"
-      }
+    if (params[["perceptionModels"]] == "singleGaussian") {
+      params[["posteriorProbMethod"]] <- "qda"
+    } else if (params[["perceptionModels"]] == "GMM") {
+      params[["posteriorProbMethod"]] <- "GMM"
     }
   }
+  
+  if ("mahalanobisDistance" %in% params[["memoryIntakeStrategy"]] && !"mahalanobisProbThreshold" %in% base::names(params)) {
+    write_log("Please specify parameter mahalanobisProbThreshold correctly (number between zero and one).", params)
+    runSimulation <- FALSE
+  }
+  if ("mahalanobisDistance" %in% params[["memoryIntakeStrategy"]] && 
+      (params[["mahalanobisProbThreshold"]] <= 0 || params[["mahalanobisProbThreshold"]] > 1)) {
+    write_log("Please specify parameter mahalanobisProbThreshold correctly (number between zero and one).", params)
+    runSimulation <- FALSE
+  }
 
-  params[["nrOfInteractions"]] <- params[["nrOfSnapshots"]] * params[["interactionsPerSnapshot"]]
-
-  if (base::is.null(params[["perceptionOVNN"]])) {
+  if ("posteriorProbThr" %in% params[["memoryIntakeStrategy"]] && !"posteriorProbThr" %in% base::names(params)) {
+    write_log("Please specify parameter posteriorProbThr correctly (number between zero and one).", params)
+    runSimulation <- FALSE
+  }
+  if ("posteriorProbThr" %in% params[["memoryIntakeStrategy"]] && 
+      (params[["posteriorProbThr"]] <= 0 || params[["posteriorProbThr"]] > 1)) {
+    write_log("Please specify parameter posteriorProbThr correctly (number between zero and one).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"perceptionOVNN" %in% base::names(params) || params[["perceptionOVNN"]] <= 0) {
     params[["perceptionOVNN"]] <- 5
   }
   if (params[["perceptionOVNN"]] %% 2 == 1) {
     params[["perceptionOVNN"]] <- params[["perceptionOVNN"]] + 1
   }
-
-  methodReg <- get_method_register()
-  if (!"featureExtractionMethod" %in% base::names(params)) {
-    params[["featureExtractionMethod"]] <- "identity"
-  }
-  if (!params[["featureExtractionMethod"]] %in% methodReg$method) {
+  
+  if (!"computeGMMsInterval" %in% base::names(params) || params[["computeGMMsInterval"]] <= 0) {
+    write_log("Please specify parameter computeGMMsInterval correctly (number higher than zero).", params)
     runSimulation <- FALSE
   }
-
-  if (params[["proportionGroupTokens"]] != 0) {
-    groupData <- input.df %>% 
-      dplyr::group_by(group) %>% 
-      dplyr::summarise(nInputGroupAvailable = dplyr::n())
-    df <- input.df %>% 
-      dplyr::group_by(speaker, group) %>% 
-      dplyr::summarise(nInput = dplyr::n()) %>%
-      dplyr::mutate(nInputGroupNeeded = base::ceiling(nInput * params[["proportionGroupTokens"]])) %>%
-      dplyr::left_join(groupData, by = "group") %>% 
-      dplyr::mutate(nInputGroupAvailable = nInputGroupAvailable - nInput)
-    if (base::any(df$nInputGroupNeeded > df$nInputGroupAvailable)) {
-      runSimulation <- FALSE
-    }
+  
+  if (params[["perceptionModels"]] == "GMM" && !"purityRepetitions" %in% base::names(params)) {
+    params[["purityRepetitions"]] <- 5
   }
-
-  if (params[["computeGMMsInterval"]] != params[["computeFeaturesInterval"]]) {
-    params[["computeGMMsInterval"]] <- params[["computeFeaturesInterval"]]
+  if (params[["perceptionModels"]] == "GMM" && params[["purityRepetitions"]] <= 0) {
+    write_log("Please specify parameter purityRepetitions correctly (number higher than zero).", params)
+    runSimulation <- FALSE
   }
   
+  if (params[["perceptionModels"]] == "GMM" && !"purityThreshold" %in% base::names(params)) {
+    params[["purityThreshold"]] <- 0.75
+  }
+  if (params[["perceptionModels"]] == "GMM" && (params[["purityThreshold"]] <= 0 || params[["purityThreshold"]] > 1)) {
+    write_log("Please specify parameter purityThreshold correctly (number between zero and one).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"forgettingRate" %in% base::names(params)) {
+    params[["forgettingRate"]] <- 1
+  }
+  if (params[["forgettingRate"]] <= 0 || params[["forgettingRate"]] > 1) {
+    write_log("Please specify parameter forgettingRate correctly (number between zero and one).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"memoryRemovalStrategy" %in% base::names(params) || params[["memoryRemovalStrategy"]] != "random") {
+    params[["memoryRemovalStrategy"]] <- "random"
+  }
+  
+  if (!"interactionPartners" %in% base::names(params)) {
+    write_log("Please specify parameter interactionPartners correctly (either
+              'betweenGroups' or 'withinGroups' or 'random').", params)
+    runSimulation <- FALSE
+  }
+  if (!base::any(base::c("betweenGroups", "withinGroups", "random") %in% params[["interactionPartners"]])) {
+    write_log("Please specify parameter interactionPartners correctly (either
+              'betweenGroups' or 'withinGroups' or 'random').", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"speakerProb" %in% base::names(params)) {
+    params[["speakerProb"]] <- NULL
+  }
+  
+  if (!"listenerProb" %in% base::names(params)) {
+    params[["listenerProb"]] <- NULL
+  }
+  
+  if (!"runMode" %in% base::names(params)) {
+    write_log("Please specify parameter runMode correctly (either 'single' or 'multiple').", params)
+    runSimulation <- FALSE
+  }
+  if (!base::any(base::c("single", "multiple") %in% params[["runMode"]])) {
+    write_log("Please specify parameter runMode correctly (either 'single' or 'multiple').", params)
+    runSimulation <- FALSE
+  }
+  
+  if (params[["runMode"]] == "multiple" && (!"multipleABMRuns" %in% base::names(params) || params[["multipleABMRuns"]] <= 0)) {
+    params[["multipleABMRuns"]] <- 1
+  }
+  
+  if (!"nrOfSnapshots" %in% base::names(params) || params[["nrOfSnapshots"]] <= 0) {
+    write_log("Please specify parameter nrOfSnapshots correctly (number higher than zero).", params)
+    runSimulation <- FALSE
+  }
+  
+  if (!"interactionsPerSnapshot" %in% base::names(params) || params[["interactionsPerSnapshot"]] < 0) {
+    write_log("Please specify parameter interactionsPerSnapshot correctly (zero or higher than zero).", params)
+    runSimulation <- FALSE
+  }
+  
+  params[["nrOfInteractions"]] <- params[["nrOfSnapshots"]] * params[["interactionsPerSnapshot"]]
+
   return(base::list(params = params, runSimulation = runSimulation))
 }
