637c25cdaac341f14af606c908060259e73352cc
Johanna Cronenberg <johanna.cronenberg@campus.lmu.de>
2021-10-23 15:38:47 +0200

    added progress bar when runMode: single

--- R/perform_interactions.R
+++ R/perform_interactions.R
@@ -4,7 +4,9 @@ perform_interactions <- function(pop, logDir, params) {
     data.table::data.table(agentID = agent$agentID, group = agent$group)
     }))[base::order(agentID),]
 
+  pb <- utils::txtProgressBar(min = 0, max = params[["nrOfSnapshots"]], initial = 0, style = 3)
   for (snap in 1:params[["nrOfSnapshots"]]) {
+    utils::setTxtProgressBar(pb, snap)
     interactionsLog <- create_interactions_log(params[["interactionsPerSnapshot"]])
     for (i in 1:params[["interactionsPerSnapshot"]]) {
       perform_single_interaction(pop, interactionsLog, snap, groupsInfo, params)
@@ -12,4 +14,5 @@ perform_interactions <- function(pop, logDir, params) {
     save_population(pop, extraCols = base::list(snapshot = snap), logDir = logDir)
     save_interactions_log(interactionsLog, extraCols = base::list(snapshot = snap), logDir = logDir)
   }
+  close(pb)
 }
