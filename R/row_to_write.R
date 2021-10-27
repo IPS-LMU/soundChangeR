061a6530af075956efd6d5e98062e029a3ee5c43
Johanna Cronenberg <johanna.cronenberg@campus.lmu.de>
2021-10-27 11:17:59 +0200

    removed options outlierRemoval and timeDecay for forgetting, thereby removing also emuR as imported package

--- R/row_to_write.R
+++ R/row_to_write.R
@@ -2,7 +2,7 @@ row_to_write <- function(agent, producedToken, params) {
 
   if (base::all(agent$memory$valid)) {
     base::print(base::paste("agent", agent$agentID, "full"))
-    rowToWrite <- row_to_overwrite(agent, producedToken, params)
+    rowToWrite <- base::sample(base::which(agent$memory$word == producedToken$word), 1)
   } else {
     rowToWrite <- base::which(agent$memory$valid == FALSE)[1]
   }
