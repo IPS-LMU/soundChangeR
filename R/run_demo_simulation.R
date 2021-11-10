6a76c48d5059696b585e8f49606579e464e6b2e1
Johanna Cronenberg <johanna.cronenberg@campus.lmu.de>
2021-11-10 10:43:39 +0100

    updated dataframe in extdata and changed run_demo_simulation() accordingly; also removed params.yaml

--- R/run_demo_simulation.R
+++ R/run_demo_simulation.R
@@ -5,9 +5,9 @@ run_demo_simulation <- function() {
   demoInputDataFile <- base::system.file("extdata", "u_fronting.csv", package = "soundChangeR")
   demoFeatures <- c("DCT0", "DCT1", "DCT2")
   demoGroup <- "age"
-  demoPhoneme <- "phoneme"
+  demoPhoneme <- "label"
   demoWord <- "word"
-  demoSpeaker <- "spk"
+  demoSpeaker <- "speaker"
   run_simulation(inputDataFile = demoInputDataFile,
                  features = demoFeatures,
                  group = demoGroup,
