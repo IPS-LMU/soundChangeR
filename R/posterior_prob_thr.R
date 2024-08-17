08bf79cce9324db92bb89b88a8721f4a19b2a4b0
Johanna Cronenberg <johanna.cronenberg@campus.lmu.de>
2024-08-17 18:07:00 +0200

    fixed bug in posteriorProbThreshold computation, then increased version to 1.0.4

--- R/posterior_prob_thr.R
+++ R/posterior_prob_thr.R
@@ -1,5 +1,5 @@
 posterior_prob_thr <- function(exemplar, features, phoneme, agent, params) {
   
   posteriorProb <- compute_posterior_probabilities(agent, features, params[["posteriorProbMethod"]])
-  recognize_posterior_probabilities(posteriorProb, phoneme, "posteriorProbThr", posteriorProbThr = params[["posteriorProbThreshold"]])
+  recognize_posterior_probabilities(posteriorProb, phoneme, "posteriorProbThr", params)
 }
--- R/recognize_posterior_probabilities.R
+++ R/recognize_posterior_probabilities.R
@@ -1,8 +1,8 @@
-recognize_posterior_probabilities <- function(posteriorProb, phoneme, method, ...) {
+recognize_posterior_probabilities <- function(posteriorProb, phoneme, method, params) {
   
   if (method == "maxPosteriorProb") {
     base::colnames(posteriorProb)[base::which.max(posteriorProb)] == phoneme
   } else if (method == "posteriorProbThr") {
-    posteriorProb[, phoneme] >= base::list(...)[["posteriorProbThreshold"]]
+    posteriorProb[, phoneme] >= params[["posteriorProbThreshold"]]
   }
 }
